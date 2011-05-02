d732c7440bdf4958cc9cd0c5455fb06ae1d92dd8
yokoyama <yokoyama@192.168.11.202>
2011-05-02 13:43:23 +0900

    タスク情報取得テスト追加

--- src/jp/sahana/chugokugtug/data/test/ProjectListFactoryTest.java
+++ src/jp/sahana/chugokugtug/data/test/ProjectListFactoryTest.java
@@ -21,7 +21,7 @@ public class ProjectListFactoryTest extends TestCase {
 		ProjectListFactory factory = new ProjectListFactory();
 		ProjectList list = (ProjectList)factory.create(client.getResponse());
 		ArrayList<Project> prjList = list.getArray();
-		assertEquals(prjList.size(), 2);
+		assertEquals(prjList.size(), 3);
 		
 		Project prj = prjList.get(0);
 		assertEquals(prj.getStatus(), 1);
@@ -32,6 +32,19 @@ public class ProjectListFactoryTest extends TestCase {
 		assertEquals(prj.getCreateDate(), "2011-03-12T15:08:46Z");
 		assertEquals(prj.getModifyDate(), "2011-03-12T15:08:46Z");
 		assertEquals(prj.getName(), "臨時災害FMラジオ放送局：岩手県奥州市:77.8MHz");
+		
+		Project prj2 = prjList.get(2);
+		assertEquals(prj2.getStatus(), 1);
+		assertEquals(prj2.getStatusName(), "アクティブ");
+		assertEquals(prj2.getUrl(), "http://sahana.jp:80/eden/project/project/3");
+		assertEquals(prj2.getCreateBy(), "tksyokoyama@gmail.com");
+		assertEquals(prj2.getModifiedBy(), "tksyokoyama@gmail.com");
+		assertEquals(prj2.getCreateDate(), "2011-05-01T09:11:58Z");
+		assertEquals(prj2.getModifyDate(), "2011-05-01T09:11:58Z");
+		assertEquals(prj2.getName(), "Androidクライアント作成プロジェクト");
+		assertEquals(prj2.getStartDate(), "2011-05-01");
+		assertEquals(prj2.getEndDate(), "2011-05-08");
+		
 	}
 
 }
---
+++ src/jp/sahana/chugokugtug/data/test/ProjectTaskFactoryTest.java
@@ -0,0 +1,38 @@
+package jp.sahana.chugokugtug.data.test;
+
+import org.apache.http.HttpStatus;
+
+import jp.sahana.chugokugtug.data.ProjectTask;
+import jp.sahana.chugokugtug.data.ProjectTaskList;
+import jp.sahana.chugokugtug.data.ProjectTaskListFactory;
+import jp.sahana.chugokugtug.web.SahanaHttpClient;
+import junit.framework.TestCase;
+
+public class ProjectTaskFactoryTest extends TestCase {
+
+	public void testCreate() {
+		SahanaHttpClient client = new SahanaHttpClient();
+		int iRet = client.getData("http://sahana.jp/eden/project/project/3/task.xml");
+		assertEquals(iRet, HttpStatus.SC_OK);
+		
+		ProjectTaskListFactory factory = new ProjectTaskListFactory();
+		ProjectTaskList tasklist = (ProjectTaskList)factory.create(client.getResponse());
+		
+		assertEquals(tasklist.getArray().size(), 2);
+		
+		ProjectTask task = tasklist.getArray().get(0);
+		assertEquals(task.getCreateDate(), "2011-05-02T02:44:52Z");
+		assertEquals(task.getModifyDate(), "2011-05-02T02:44:52Z");
+		assertEquals(task.getCreatedBy(), "tksyokoyama@gmail.com");
+		assertEquals(task.getModifiedBy(), "tksyokoyama@gmail.com");
+		assertEquals(task.getUrl(), "http://sahana.jp:80/eden/project/project/3/task/1");
+		assertEquals(task.isUrgent(), false);
+		assertEquals(task.getPriority(), 1);
+		assertEquals(task.getPriorityName(), "通常");
+		assertEquals(task.getSubject(), "タスク一覧表示作成");
+		assertEquals(task.getDescription(), "タスク一覧表示機能を作る");
+		assertEquals(task.getStatus(), 1);
+		assertEquals(task.getStatusName(), "新規登録");
+	}
+
+}
